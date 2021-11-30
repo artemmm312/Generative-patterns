c1c110176fdd38544ec9cd6e77383e99d38bd80f
artemmm312 <artemmm312@gmail.com>
2021-11-30 19:13:36 +0300

    fix

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-Prototype',
+        'version' => 'dev-Prototype',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '891d8749dad18f84573e42ed03d1b9f4b81d9461',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-Prototype',
+            'version' => 'dev-Prototype',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '891d8749dad18f84573e42ed03d1b9f4b81d9461',
             'dev_requirement' => false,
         ),
     ),
